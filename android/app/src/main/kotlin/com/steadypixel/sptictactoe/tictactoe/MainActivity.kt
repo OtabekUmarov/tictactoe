852d72e498de5f809551186245e22296ca826ebc
Azizbek <Azizbekshamsiyev946@gmail.com>
2024-12-21 11:51:00 +0500

    rename

---
+++ android/app/src/main/kotlin/com/steadypixel/sptictactoe/tictactoe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.steadypixel.sptictactoe.tictactoe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tictactoe/pages/intro_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF0A1314),
+      ),
+      themeMode: ThemeMode.dark,
+      home: IntroPage(),
+    );
+  }
+}
